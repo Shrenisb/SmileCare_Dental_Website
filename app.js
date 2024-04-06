5c81ab162bbab9045f1f89e5d77855460e71186a
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-04-07 00:53:50 +0530

    init hbs

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const mongoose = require('mongoose')
 const body = require('body-parser')
 const app = express()
 const { MongoClient } = require('mongodb')
+
+const handlebars = require("express-handlebars");
+//Sets our app to use the handlebars engine
+app.set("view engine", "handlebars");
+//Sets handlebars configurations (we will go through them later on)
+app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.use(express.static('public', { setHeaders: (res, path) => {
+  if (path.endsWith('.png')) {
+    res.setHeader('Content-Type', 'image/png');
+  }
+}}));  
+
 app.listen(3000, async () => {
     await connectDB();
     console.log(`http://localhost:3000`);
