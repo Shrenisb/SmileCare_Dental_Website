7d9d3d2a4db13a9f100a4affb6b1f4ab423c6124
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-08-02 19:13:24 +0530

    organization

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,9 @@
 'use strict';
 
-/* addEvent on element*/
+const nav = document.querySelector('.mobile-nav');
+const navMenuBtn = document.querySelector('.nav-menu-btn');
+const navCloseBtn = document.querySelector('.nav-close-btn');
+const navToggleFunc = function () { nav.classList.toggle('active'); }
 
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
-    }
-  } else {
-    elem.addEventListener(type, callback);
-  }
-}
-
-/*navbar toggle*/
-
-const navbar = document.querySelector("[data-navbar]");
-const navbarLinks = document.querySelectorAll("[data-nav-link]");
-const navbarToggler = document.querySelector("[data-nav-toggler]");
-
-const toggleNav = function () {
-  navbar.classList.toggle("active");
-  navbarToggler.classList.toggle("active");
-}
-
-addEventOnElem(navbarToggler, "click", toggleNav);
-
-const closeNav = function () {
-  navbar.classList.remove("active");
-  navbarToggler.classList.remove("active");
-}
-
-addEventOnElem(navbarLinks, "click", closeNav);
-
-/*header active*/
-
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
-
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-});
+navMenuBtn.addEventListener('click', navToggleFunc);
+navCloseBtn.addEventListener('click', navToggleFunc);
